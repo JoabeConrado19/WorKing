be82fab2aae6b3ed19a63ff3143b3cdb47a3933f
Filipe Lucena <filipelucena1@gmail.com>
2022-11-01 13:34:33 -0300

    Fix: resolving conflits

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,6 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { LandingPage } from '../Pages/LandingPage';
-
+import { Register } from '../Pages/registerPage';
 
 const RoutesMain = () => {
         
@@ -9,7 +9,7 @@ const RoutesMain = () => {
             <Route path='/' element={ <LandingPage/> } />
             <Route path='/contacts' element={ <h1>Fale conosco</h1> } />
             <Route path='/about-us' element={ <h1>Sobre nós</h1> } />
-            <Route path='/register' element={ <h1>Register</h1> } />
+            <Route path='/register' element={ <Register/> } />
             <Route path='/login' element={ <h1>Login</h1> } />
             <Route path='/dashboard' element={ <h1>Dashboard</h1> } />
             <Route path='*' element={ <h2>Not Found</h2> } />
