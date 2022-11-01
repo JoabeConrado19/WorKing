f7187fc27b6ad578a2430796879ca634cbe0966a
Filipe Lucena <filipelucena1@gmail.com>
2022-11-01 13:55:42 -0300

    Fix: remove landingpage bugs

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import { LandingPage } from '../pages/LandingPage';
-import { Register } from '../pages/registerPage';
 
 const RoutesMain = () => {
         
@@ -9,7 +8,7 @@ const RoutesMain = () => {
             <Route path='/' element={ <LandingPage/> } />
             <Route path='/contacts' element={ <h1>Fale conosco</h1> } />
             <Route path='/about-us' element={ <h1>Sobre nós</h1> } />
-            <Route path='/register' element={ <Register/> } />
+            <Route path='/register' element={ <h1>Register</h1> } />
             <Route path='/login' element={ <h1>Login</h1> } />
             <Route path='/dashboard' element={ <h1>Dashboard</h1> } />
             <Route path='*' element={ <h2>Not Found</h2> } />
