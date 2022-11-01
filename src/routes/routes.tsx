7dac7000d82242951407acea4c2f26b78616d90d
JoabeConrado19 <joabe.conrado19@gmail.com>
2022-11-01 01:28:13 -0400

    Feat: GlobalStyles

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Register } from '../pages/registerPage';
 
 
 const RoutesMain = () => {
         
     return(  <Routes >
           <Route path='/' element={ <h1>LandingPage</h1> } />
-          <Route path='/register' element={ <h1>Register</h1> } />
+          <Route path='/register' element={ <Register/> } />
           <Route path='/login' element={ <h1>Login</h1> } />
           <Route path='/dashboard' element={ <h1>Dashboard</h1> } />
           <Route path='*' element={ <h2>Not Found</h2> } />
