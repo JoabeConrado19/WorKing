1b34857acf411c033fbf9d13f99b942f1ecbb3d5
JoabeConrado19 <joabe.conrado19@gmail.com>
2022-11-02 15:47:13 -0400

    Fix: Merge

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,7 @@ import { DashboardClient } from '../Pages/DashboardClient';
 import { LandingPage } from '../Pages/LandingPage';
 import { Register } from '../Pages/registerPage';
 import { RegisterProfessional } from '../Pages/registerProfessional';
+import { LoginPage } from '../Pages/LoginPage';
 
 const RoutesMain = () => {
 
@@ -10,7 +11,7 @@ const RoutesMain = () => {
     <Route path='/' element={<LandingPage />} />
     <Route path='/client-register' element={<Register />} />
     <Route path='/worker-register' element={<RegisterProfessional />} />
-    <Route path='/login' element={<h1>Login</h1>} />
+    <Route path='/login' element={<LoginPage />} />
     <Route path='/dashboard' element={<DashboardClient />} />
     <Route path='/contacts' element={<h1>Fale conosco</h1>} />
     <Route path='/about-us' element={<h1>Sobre nós</h1>} />
