bc6ad89503a745b87050ad31a8cd75f05bc095f3
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2022-11-04 11:41:01 -0400

    add: about us page

---
+++ src/components/AboutUsPage/aside/style.ts
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+
+export const Aside = styled.aside`
+  height: 100vh;
+  background-color: #ffa800;
+  position: fixed;
+  z-index: 2;
+
+  .userName {
+    height: 12%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    svg {
+      font-size: 50px;
+    }
+
+    h2 {
+      width: 0px;
+      padding: 0px;
+      visibility: hidden;
+    }
+  }
+
+  .container__menu {
+    width: 100%;
+    height: 88%;
+  }
+
+  .menu {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    li {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: start;
+      padding: 10px 10px 10px 20px;
+
+      svg {
+        font-size: 30px;
+      }
+
+      p {
+        visibility: hidden;
+        margin-left: 10px;
+        width: 0px;
+      }
+
+      :hover {
+        background-color: #ffffff;
+      }
+    }
+  }
+
+  :hover {
+    width: 15vw;
+    position: absolute;
+    z-index: 2;
+
+    .userName {
+      width: 100%;
+      height: 20%;
+      flex-direction: column;
+
+      svg {
+        font-size: 70px;
+      }
+
+      h2 {
+        width: 100%;
+        padding: 10px 0;
+        visibility: visible;
+        text-align: center;
+      }
+    }
+
+    .container__menu {
+      height: 80%;
+
+      .menu {
+        li {
+          p {
+            visibility: visible;
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+`;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,24 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-import { DashboardClient } from '../Pages/DashboardClient';
-import { LandingPage } from '../Pages/LandingPage';
-import { Register } from '../Pages/registerPage';
-import { RegisterProfessional } from '../Pages/registerProfessional';
+import { Route, Routes } from "react-router-dom";
+import { AboutUsPage } from "../Pages/AboutUsPage";
+import { DashboardClient } from "../Pages/DashboardClient";
+import { LandingPage } from "../Pages/LandingPage";
+import { Register } from "../Pages/registerPage";
+import { RegisterProfessional } from "../Pages/registerProfessional";
 
 const RoutesMain = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/client-register" element={<Register />} />
+      <Route path="/worker-register" element={<RegisterProfessional />} />
+      <Route path="/login" element={<h1>Login</h1>} />
+      <Route path="/dashboard" element={<DashboardClient />} />
+      <Route path="/contacts" element={<h1>Fale conosco</h1>} />
+      <Route path="/about-us" element={<AboutUsPage />} />
+      <Route path="/register" element={<h1>Register</h1>} />
+      <Route path="*" element={<h2>Not Found</h2>} />
+    </Routes>
+  );
+};
 
-  return (<Routes >
-    <Route path='/' element={<LandingPage />} />
-    <Route path='/client-register' element={<Register />} />
-    <Route path='/worker-register' element={<RegisterProfessional />} />
-    <Route path='/login' element={<h1>Login</h1>} />
-    <Route path='/dashboard' element={<DashboardClient />} />
-    <Route path='/contacts' element={<h1>Fale conosco</h1>} />
-    <Route path='/about-us' element={<h1>Sobre nós</h1>} />
-    <Route path='/register' element={<h1>Register</h1>} />
-    <Route path='*' element={<h2>Not Found</h2>} />
-
-  </Routes>)
-
-}
-
-export default RoutesMain
+export default RoutesMain;
