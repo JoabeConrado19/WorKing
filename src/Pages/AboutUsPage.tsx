9b9254bf7f694b9bb6eed8930e493a987242d14f
JoabeConrado19 <joabe.conrado19@gmail.com>
2022-11-06 21:20:04 -0400

    Hotfix: about us route

--- src/Pages/AboutUsPage.tsx
+++ src/Pages/AboutUsPage.tsx
@@ -1,17 +1,12 @@
 import { Body } from "../Components/AboutUsPage/body";
 import { AsideComponent } from "../Components/AboutUsPage/aside";
 import { Main } from "../Components/AboutUsPage/main";
-import { useContext, useEffect, useState } from "react";
-import { DashboardContext } from "../contexts/dashboard";
 
 export const AboutUsPage = () => {
 
-  const {verifyToken}:any = useContext(DashboardContext)
 
 
-  useEffect(()=>{
-    verifyToken()
-  },[])
+
   
 
   return (
